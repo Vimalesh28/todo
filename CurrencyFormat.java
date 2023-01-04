4ff4ad2d5fd945973e709e683bc4180a5cecf8f0
Vimalesh28 <vimaleshkvkl@gmail.com>
2023-01-04 14:51:18 +0530

    added new list

--- CurrencyFormat.java
+++ CurrencyFormat.java
@@ -1,4 +1,5 @@
-https://www.hackerrank.com/challenges/java-currency-formatter/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+//https://www.hackerrank.com/challenges/java-currency-formatter/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+package samm;
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -8,22 +9,22 @@ import java.util.regex.*;
 public class CurrencyFormat {
     
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double payment = scanner.nextDouble();
-        scanner.close();
-      NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US);
-        String us = n.format(payment);
-       NumberFormat n1 = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
-        String india = n1.format(payment);
-        NumberFormat n2 = NumberFormat.getCurrencyInstance(Locale.CHINA);
-        String china = n2.format(payment);
-        NumberFormat n3 = NumberFormat.getCurrencyInstance(Locale.FRANCE);
-        String france = n3.format(payment);
+        Scanner sc=new Scanner(System.in);
+        List l=new ArrayList();
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++) {
+        	arr[i]=sc.nextInt();
+        }
+        int Ino = sc.nextInt();
+        int Ino2=sc.nextInt();
+        int dno=sc.nextInt();
+        	arr[Ino]=Ino2;
         
+       for(int i=0;i<arr.length;i++) {
+    	l.add(arr[i]);   
+       }
+       
+       System.out.print(l);
         
-         System.out.println("US: " + us);
-        System.out.println("India: " + india);
-        System.out.println("China: " + china);
-        System.out.println("France: " + france);
-    }
-}
+    }}
